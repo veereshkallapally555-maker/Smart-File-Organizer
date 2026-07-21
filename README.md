aa88136bfd104742c38284c01023b62d786d3119
VEERESH KALLAPALLY <veereshkallapally555@gmail.com>
2026-07-21 18:58:47 +0530

    Build Smart File Organizer

---
+++ README.md
@@ -0,0 +1,64 @@
+📂 Smart File Organizer
+
+A Python automation tool that automatically organizes files into folders based on their file extensions.
+
+✨ Features
+
+- Organizes files automatically
+- Creates folders if they don't exist
+- Supports Images, Documents, Videos, Audio, and Archives
+- Handles unknown file types by placing them in an "Others" folder
+- Built using Python's `pathlib` and `shutil` modules
+
+📁 Project Structure
+
+```
+Smart-File-Organizer/
+│
+├── organizer.py
+├── README.md
+├── .gitignore
+└── test_folder/
+```
+
+🚀 How to Run
+
+1. Clone the repository.
+2. Open the project in VS Code.
+3. Run:
+
+```bash
+python organizer.py
+```
+
+4. Enter the folder path when prompted.
+
+🛠️ Technologies Used
+
+- Python 3
+- pathlib
+- shutil
+
+📚 Concepts Learned
+
+- Functions
+- Dictionaries
+- Loops
+- File Handling
+- pathlib
+- shutil
+- Git
+- GitHub
+
+🔮 Future Improvements
+
+- GUI version
+- Logging
+- Progress bar
+- Configuration file
+- Unit tests
+
+👨‍💻 Author
+**Veeresh Kallapally**
+* GitHub: [@veereshkallapally555-maker](https://github.com/veereshkallapally555-maker)
+* LinkedIn: [Veeresh Kallapally](https://www.linkedin.com/in/veeresh-kallapally-a87164390/)
---
+++ organizer.py
@@ -0,0 +1,72 @@
+import shutil
+from pathlib import Path
+
+FILE_TYPES = {
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+    ".bmp": "Images",
+
+    ".pdf": "Documents",
+    ".docx": "Documents",
+    ".txt": "Documents",
+    ".xlsx": "Documents",
+    ".pptx": "Documents",
+
+    ".mp3": "Audio",
+    ".wav": "Audio",
+    ".aac": "Audio",
+    ".flac": "Audio",
+
+    ".mp4": "Videos",
+    ".avi": "Videos",
+    ".mov": "Videos",
+    ".mkv": "Videos",
+
+    ".zip": "Archives",
+    ".rar": "Archives",
+    ".tar": "Archives",
+    ".gz": "Archives"
+}
+
+def get_category(extension):
+    return FILE_TYPES.get(extension.lower(), "Others")
+
+def move_file(file_path, category):
+    destination = file_path.parent / category
+    destination.mkdir(exist_ok=True)
+    shutil.move(str(file_path), str(destination / file_path.name))
+
+def main():
+    print("=== Smart File Organizer ===")
+
+    folder = get_folder_path()
+
+    if folder is None:
+        return
+    print("folder found: ",folder)
+
+    show_files(folder)
+
+def get_folder_path():
+    folder = input("Enter folder path: ")
+    path = Path(folder)
+    if path.exists():
+        return path
+    
+    print("folder does not exist")
+    return None
+
+def show_files(folder):
+    print("\n Files Found \n")
+
+    for item in folder.iterdir():
+        if item.is_file():
+            category = get_category(item.suffix)
+            move_file(item, category)
+            print(f"moved{item.name} --> {category}")
+
+
+if __name__ == "__main__":
+    main()
